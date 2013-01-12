cd9fe051033b08f8458208531a51b3aa552b4abc
jaybora <jbr@jbr.dk>
2013-01-12 12:20:48 +0100

    Added a simple static maps with plants

--- src/main.go
+++ src/main.go
@@ -6,8 +6,12 @@ import (
 	"web"
 	"plantdata"
 	"dataproviders"
+	"log"
 )
 
+type staticPlants struct {
+	plants map[string]plantdata.PlantData
+}
 
 // Load up an example plant
 var plantmap = map[string]plantdata.PlantData {
@@ -17,17 +21,21 @@ var plantmap = map[string]plantdata.PlantData {
 }
 
 func main() {
+	plants := staticPlants{plantmap}
 	controller := controller.NewController()
 
 	http.HandleFunc("/", web.DefaultHandler)
 	http.HandleFunc("/plant/", func(w http.ResponseWriter, r *http.Request) {
-		web.PlantHandler(w, r, &controller, )
+		web.PlantHandler(w, r, &controller, plants)
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-func PlantData(plantkey string) plantdata.PlantData {
-	return plantdata.PlantData{PlantKey: "jbr", 
-	                           Name: "Klarinetvej 25",
-	                           DataProvider: dataproviders.FJY}
+func (s staticPlants)PlantData(plantkey string) *plantdata.PlantData {
+	log.Printf("Getting plant for plantkey: %s", plantkey)
+	plant, ok := s.plants[plantkey]
+	if !ok {
+		return nil
+	}
+	return &plant;
 }
