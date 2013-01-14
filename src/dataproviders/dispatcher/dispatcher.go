a271e0799b673d6db912d449b0e533d472467a3f
jaybora <jbr@jbr.dk>
2013-01-14 22:13:48 +0100

    Added new dataprovider

--- src/dataproviders/dispatcher/dispatcher.go
+++ src/dataproviders/dispatcher/dispatcher.go
@@ -13,6 +13,10 @@ func Provider(implType int, init dataproviders.InitiateData, term dataproviders.
 			jfy := JFY.NewDataProvider(init, term)
 			provider = &jfy
 			return
+		case dataproviders.SunnyPortal:
+			sunny := sunnyportal.NewDataProvider(init, term)
+			provider = &sunny
+			return
 	}
 	err = fmt.Errorf("No provider found for %d", implType)
 	return
