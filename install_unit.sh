69678843b5967342686e93addbbf137dd8471738
Jesper Rasmussen <jbr@jbr.dk>
2013-08-13 15:34:42 +0200

    Updated to set GOPATH to pwd

--- install_unit.sh
+++ install_unit.sh
@@ -1,2 +1,2 @@
-GOPATH=/Users/jbr/github/local/solarcompare CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o dpunit_linux_386 src/dpunit/dpunit.go
-GOPATH=/Users/jbr/github/local/solarcompare go build -o dpunit_darwin src/dpunit/dpunit.go
+GOPATH=`pwd` CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o dpunit_linux_386 src/dpunit/dpunit.go
+GOPATH=`pwd` go build -o dpunit_darwin src/dpunit/dpunit.go
