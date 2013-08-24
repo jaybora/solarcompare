f08fe718658a7cf947b3bece383d005f5b6bf0b4
jaybora <jbr@jbr.dk>
2013-08-24 20:27:04 +0200

    Working on day and monthly production

--- src/serverpart/handlers/logpvdata.go
+++ src/serverpart/handlers/logpvdata.go
@@ -17,17 +17,21 @@ import (
 )
 
 const keyDateFormat = "20060102"
+const keyMonthFormat = "200601"
 
 // This is for the marshalling to json
 type LogPvDataForJson struct {
-	LogTime time.Time
-	PvData  dataproviders.PvData
+	LogTime         time.Time
+	PvData          dataproviders.PvData
+	EnergyTotalDiff float32
+	EnergyTodayDiff uint16
 }
 
 // This is stored in the datastore
 type LogPvDataDaily struct {
-	LogKey string `goon:"id"`
-	Logs   []byte //Json encoded array of LogPvDataForJson
+	LogKey string         `goon:"id"`
+	Parent *datastore.Key `datastore:"-" goon:"parent"`
+	Logs   []byte         //Json encoded array of LogPvDataForJson
 }
 
 // This handler is called by app engine cron to regular
@@ -43,6 +47,7 @@ func LogPvDataHandler(w http.ResponseWriter, r *http.Request) {
 	// Key for datastore is composite of plantkey/yyyyMMdd
 	// logtype=monthly: Log
 
+	c := appengine.NewContext(r)
 	if r.URL.Query().Get("logtype") == "daily" {
 		logDaily(w, r)
 	} else {
@@ -53,7 +58,18 @@ func LogPvDataHandler(w http.ResponseWriter, r *http.Request) {
 
 		plantkey := web.PlantKey(r.URL.String(), keypos)
 		if plantkey != "" {
-			logdatadaily := getLogDaily(&plantkey, r)
+			p, err := Plant(r, &plantkey)
+
+			if err != nil {
+				c.Infof("Could not find plat due to %s", err.Error())
+				http.Error(w, fmt.Sprintf(err.Error()),
+					http.StatusInternalServerError)
+				return
+			}
+
+			date := r.URL.Query().Get("date")
+
+			logdatadaily := getLogDaily(p, r, &date)
 			w.Header().Add("Content-type", "application/json")
 			w.Write(logdatadaily.Logs)
 			return
@@ -78,6 +94,7 @@ func logDaily(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	g := goon.NewGoon(r)
 	q := logDailyGetPlants(w, r)
+	logtime := time.Now()
 	okcounter := 0
 	for i := g.Run(q); ; {
 		p := plantdata.Plant{}
@@ -96,7 +113,7 @@ func logDaily(w http.ResponseWriter, r *http.Request) {
 		// Get daily log from datastore
 		c.Debugf("Getting PvData log for plantkey %s", p.PlantKey)
 
-		logDaily := getLogDaily(&p.PlantKey, r)
+		logDaily := getLogDaily(&p, r, nil)
 		c.Debugf("Getting PvData log for plantkey %s", p.PlantKey)
 
 		var exsistingLogs []LogPvDataForJson
@@ -115,7 +132,8 @@ func logDaily(w http.ResponseWriter, r *http.Request) {
 			c.Errorf("%s", err.Error())
 			continue
 		}
-		newLog := LogPvDataForJson{time.Now(), pvdata}
+		//Logtime to keep all logs for this jobrun syncronizes
+		newLog := LogPvDataForJson{logtime, pvdata}
 		newLogs := append(exsistingLogs, newLog)
 
 		// Make it a json string again
@@ -126,7 +144,7 @@ func logDaily(w http.ResponseWriter, r *http.Request) {
 		logDaily.Logs = b
 
 		//Put it in store
-		putLogDaily(&p.PlantKey, r, logDaily)
+		putLogDaily(&p, time.Now(), r, logDaily)
 		okcounter++
 
 	}
@@ -161,28 +179,125 @@ func getPvData(plantkey *string, r *http.Request) (pvdata dataproviders.PvData, 
 	return
 }
 
-func getLogDaily(plantkey *string, r *http.Request) *LogPvDataDaily {
+func getLogDaily(plant *plantdata.Plant, r *http.Request, date *string) *LogPvDataDaily {
 	c := appengine.NewContext(r)
 	g := goon.NewGoon(r)
 
 	l := &LogPvDataDaily{}
-	l.LogKey = fmt.Sprintf("%s/%s", *plantkey, time.Now().Format(keyDateFormat))
-	err := g.Get(l)
+	if *date == "" {
+		tmp := time.Now().Format(keyDateFormat)
+		date = &tmp
+	}
+	l.LogKey = fmt.Sprintf("%s", *date)
+	plantkey, err := g.GetStructKey(plant)
 	if err != nil {
-		c.Infof("Got an error when getting PvDataDaily from datastore, %s", err.Error())
+		c.Infof("Got an error when getting PvLogDaily from datastore, %s", err.Error())
+	}
+	l.Parent = plantkey
+	err = g.Get(l)
+	if err != nil {
+		c.Infof("Got an error when getting PvLogDaily from datastore, %s", err.Error())
 	}
 	return l
 }
 
-func putLogDaily(plantkey *string, r *http.Request, l *LogPvDataDaily) error {
+func putLogDaily(plant *plantdata.Plant, date time.Time, r *http.Request, l *LogPvDataDaily) error {
 	c := appengine.NewContext(r)
 	g := goon.NewGoon(r)
 
-	l.LogKey = fmt.Sprintf("%s/%s", *plantkey, time.Now().Format(keyDateFormat))
-	err := g.Put(l)
+	// l.LogKey = fmt.Sprintf("%s/%s", plant.PlantKey, time.Now().Format(keyDateFormat))
+	// err := g.Put(l)
+	// if err != nil {
+	// 	c.Infof("Got an error when putting PvDataDaily to datastore, %s", err.Error())
+	// 	return err
+	// }
+
+	// Put with plant as ancestor (parent)
+	l.LogKey = fmt.Sprintf("%s", date.Format(keyDateFormat))
+	plantkey, err := g.GetStructKey(plant)
+	l.Parent = plantkey
+	if err != nil {
+		c.Infof("Got an error when putting PvDataDaily to datastore, %s", err.Error())
+		return err
+	}
+	err = g.Put(l)
 	if err != nil {
 		c.Infof("Got an error when putting PvDataDaily to datastore, %s", err.Error())
 		return err
 	}
 	return nil
 }
+
+// func moveOldPvLogDaily(w http.ResponseWriter, r *http.Request) {
+// 	// All plants
+// 	c := appengine.NewContext(r)
+// 	g := goon.NewGoon(r)
+// 	q := logDailyGetPlants(w, r)
+
+// 	for i := g.Run(q); ; {
+// 		p := plantdata.Plant{}
+// 		_, err := i.Next(&p)
+// 		if err == datastore.Done {
+// 			break
+// 		}
+// 		if err != nil {
+// 			c.Infof("Moveoldpvlog: Could not list plants for dailylog from datastore due to %s", err.Error())
+// 			http.Error(w, fmt.Sprintf(err.Error()),
+// 				http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		// OK p is now a plant.
+// 		// Go through dates, start at 20130715
+// 		//curdate := getDate(2013, 07, 15)
+// 		enddate := getDate(2013, time.August, 18)
+// 		for curdate := getDate(2013, time.July, 15); curdate.Before(enddate); curdate = curdate.AddDate(0, 0, 1) {
+// 			// Get the old log
+// 			l := &LogPvDataDaily{}
+// 			l.LogKey = fmt.Sprintf("%s/%s", p.PlantKey, curdate.Format(keyDateFormat))
+// 			err = g.Get(l)
+// 			if err != nil {
+// 				c.Infof("Moveoldpvlog: Got an error when getting PvLogDaily from datastore, %s", err.Error())
+// 				continue
+// 			}
+// 			c.Debugf("Now moving pvlog for plant %s, for date %s...",
+// 				p.PlantKey, curdate.Format(keyDateFormat))
+
+// 			newl := &LogPvDataDaily{}
+
+// 			newl.Logs = l.Logs
+
+// 			//Put the new log
+// 			err = putLogDaily(&p, curdate, r, newl)
+// 			if err != nil {
+// 				c.Infof("Moveoldpvlog: Got an error when puttinh PvLogDaily to datastore, %s", err.Error())
+// 			} else {
+// 				c.Debugf("Finished moving pvlog for plant %s, for date %s", p.PlantKey,
+// 					curdate.Format(keyDateFormat))
+// 			}
+
+// 			//curdate = curdate.AddDate(0, 0, 1)
+// 		}
+
+// 	}
+
+// }
+
+func getDate(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
+
+/* Princip for log monthly
+ * This will go log all day productions for a given month.
+ * If run without parameters, default from app engine cron
+ * then update for the current month.
+ * Go through all plants
+ * - Get existing monthlogrecord from datastore. Create if dont exists
+ * - - Add logrecord to json for days missing before now. Calculate production by total production value minus previous days value
+ * - Close logrecord and switch plant
+ */
+
+// func logMonthly(w http.ResponseWriter, r *http.Request) {
+// 	c := appengine.NewContext(r)
+// 	g := goon.NewGoon(r)
+// }
